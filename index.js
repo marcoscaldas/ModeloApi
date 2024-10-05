c40b043631b950ba3864c05682fe42f110e442d1
marcoscaldas <donarumo@hotmail.com>
2024-10-05 19:40:27 -0300

    atualizacao de codigos

--- index.js
+++ index.js
@@ -1,74 +1,72 @@
 const express = require('express');
-const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const { Pool } = require('pg')
-    
+const pg = require('pg');
+const { Pool } = pg;
+
+
 const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+  connectionString: process.env.POSTGRES_URL,  // Verifique o nome correto
 });
 
 pool.connect((err) => {
-    if (err) throw err;
-    console.log('Conectado ao MySQL!');
+  if (err) throw err;
+  console.log('Conectado ao PostgreSQL!');
 });
 
 // Rota para obter todos os dados (READ)
 app.get('/api/dados', (req, res) => {
-    pool.query('SELECT * FROM dados', (err, results) => {
-        if (err) throw err;
-        res.json(results);
-    });
+  pool.query('SELECT * FROM dados', (err, results) => {
+    if (err) throw err;
+    res.json(results.rows);
+  });
 });
 
 // Rota para inserir novo dado (CREATE)
 app.post('/api/dados', (req, res) => {
-    const novoDado = req.body;
-    pool.query('INSERT INTO dados SET ?', novoDado, (err, result) => {
-        if (err) {
-            return res.status(500).send(err);
-        }
-        res.status(201).send({ id: result.insertId, ...novoDado });
-    });
+  const { campo1, campo2 } = req.body;
+  pool.query('INSERT INTO dados (campo1, campo2) VALUES ($1, $2) RETURNING *', [campo1, campo2], (err, result) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.status(201).send(result.rows[0]);
+  });
 });
 
 // Rota para atualizar dado existente (UPDATE)
 app.put('/api/dados/:id', (req, res) => {
-    const id = req.params.id;
-    const dadoAtualizado = req.body;
-    pool.query('UPDATE dados SET ? WHERE id = ?', [dadoAtualizado, id], (err, result) => {
-        if (err) {
-            return res.status(500).send(err);
-        }
-        if (result.affectedRows === 0) {
-            return res.status(404).send({ message: 'Dado não encontrado' });
-        }
-        res.send({ id, ...dadoAtualizado });
-    });
+  const id = req.params.id;
+  const { campo1, campo2 } = req.body;
+  pool.query('UPDATE dados SET campo1 = $1, campo2 = $2 WHERE id = $3 RETURNING *', [campo1, campo2, id], (err, result) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    if (result.rowCount === 0) {
+      return res.status(404).send({ message: 'Dado não encontrado' });
+    }
+    res.send(result.rows[0]);
+  });
 });
 
 // Rota para deletar dado existente (DELETE)
 app.delete('/api/dados/:id', (req, res) => {
-    const id = req.params.id;
-    pool.query('DELETE FROM dados WHERE id = ?', [id], (err, result) => {
-        if (err) {
-            return res.status(500).send(err);
-        }
-        if (result.affectedRows === 0) {
-            return res.status(404).send({ message: 'Dado não encontrado' });
-        }
-        res.send({ message: 'Dado deletado com sucesso' });
-    });
+  const id = req.params.id;
+  pool.query('DELETE FROM dados WHERE id = $1 RETURNING *', [id], (err, result) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    if (result.rowCount === 0) {
+      return res.status(404).send({ message: 'Dado não encontrado' });
+    }
+    res.send({ message: 'Dado deletado com sucesso' });
+  });
 });
 
-const port = proces.env.PORT || 3001;
-
-app.listen(port, () => {
-    console.log('API rodando na porta', port);
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`API rodando na porta ${process.env.PORT || 5000}`);
 });
